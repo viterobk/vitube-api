0bc427c6ed94aeae415a1da53e88fd5d1d0ecbda
Viktor Shchelkunov <vitero@bk.ru>
2022-08-15 12:45:53 +0300

    Add videos routes

--- routes/videos.ts
+++ routes/videos.ts
@@ -1,3 +1,49 @@
+import { Route } from "../core/Route";
+import { user } from "./authStrategies";
+
 export default [
+    new Route()
+        .method('get')
+        .path('/videos')
+        .authStrategy(user)
+        .handler((_, context) => {
+            
+        })
+        .build(),
+
+    new Route()
+        .method('get')
+        .path('/videos/:videoid')
+        .authStrategy(user)
+        .handler((_, context) => {
+
+        })
+        .build(),
+
+    new Route()
+        .method('post')
+        .path('/videos')
+        .authStrategy(user)
+        .handler((_, context) => {
+
+        })
+        .build(),
     
+    new Route()
+        .method('put')
+        .path('/videos/:videoid')
+        .authStrategy(user)
+        .handler((_, context) => {
+
+        })
+        .build(),
+
+    new Route()
+        .method('delete')
+        .path('/videos/:videoid')
+        .authStrategy(user)
+        .handler((_, context) => {
+
+        })
+        .build(),
 ];
